61026daaacf043e22ba035176aa4ddded269f107
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-03-07 12:02:04 -0300

    shopping Cart

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -1,6 +1,10 @@
-import { CardFooter, CoffeeCardContainer, Description, Name, Tags } from "./styles";
+import { AddCartWrapper, CardFooter, CoffeeCardContainer, Description, Name, Tags } from "./styles";
 import Coffee01 from '../../../../assets/Coffee-01.png'
 import { RegularText, TitleText } from "../../../../components/typography";
+import { QuantityInput } from "../QuantityInput";
+import { ShoppingCart } from "phosphor-react";
+
+
 export function CoffeeCard() {
     return (
         <CoffeeCardContainer>
@@ -16,6 +20,13 @@ export function CoffeeCard() {
                     <RegularText size="s">R$</RegularText>
                     <TitleText size="m" color="text" as="strong">9,90</TitleText>
                 </div>
+
+                <AddCartWrapper>
+                    <QuantityInput />
+                    <button>
+                        <ShoppingCart size={22} weight="fill"/>
+                    </button>
+                </AddCartWrapper>
             </CardFooter>
         </CoffeeCardContainer>
     )
---
+++ src/pages/Home/components/QuantityInput/index.tsx
@@ -0,0 +1,7 @@
+import { QuantityInputContainer } from "./styles";
+
+export function QuantityInput( ) {
+    <QuantityInputContainer>
+        <input type="number" readOnly value={1}/>
+    </QuantityInputContainer>
+}
